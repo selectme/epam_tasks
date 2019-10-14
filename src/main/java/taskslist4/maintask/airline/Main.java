27e970caa88efc0b06701f88c093af7fc8abb294
Viktar Mudrahelau <prespo@tut.by>
2019-10-14 17:24:39 +0300

    fixed some bugs

--- src/main/java/taskslist4/maintask/airline/Main.java
+++ src/main/java/taskslist4/maintask/airline/Main.java
@@ -2,6 +2,7 @@ package taskslist4.maintask.airline;
 
 import taskslist4.maintask.airline.model.Airplane;
 import taskslist4.maintask.airline.service.impl.AirlineServiceImpl;
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -12,7 +13,8 @@ public class Main {
 
     public static void main(String[] args) {
         AirlineServiceImpl service = new AirlineServiceImpl();
-        List<Airplane> airplanes = service.createAirplanesList(FILENAME);
+        List<Airplane> airplanes = new ArrayList<>();
+        airplanes = service.createAirplanesList(FILENAME);
 
     }
 }
