af8ea81741e117da190ce68aac12ed246c346d0c
Viktar Mudrahelau <prespo@tut.by>
2019-10-14 17:23:32 +0300

    edited tests

--- src/test/java/taskslist4/maintask/airline/service/impl/AirlineServiceImplTest.java
+++ src/test/java/taskslist4/maintask/airline/service/impl/AirlineServiceImplTest.java
@@ -5,10 +5,10 @@ import org.junit.Test;
 import taskslist4.maintask.airline.model.Airplane;
 import taskslist4.maintask.airline.model.AirplaneType;
 import taskslist4.maintask.airline.model.CargoAirplane;
+import taskslist4.maintask.airline.model.PassengerAirplane;
 import taskslist4.maintask.airline.service.AirlineService;
 
-import java.io.File;
-import java.net.URL;
+import java.util.ArrayList;
 import java.util.List;
 
 import static org.junit.Assert.*;
@@ -20,92 +20,96 @@ public class AirlineServiceImplTest {
     private static final String RIGHT_DATA = "airplanesRightData.txt";
     private static final String WRONG_DATA = "airplanesWrongData.txt";
     private AirlineService airlineService;
-
+    private List<Airplane> airplanesRightData;
+    private List<Airplane> airplanesWrongData;
+    private List<Airplane> airplanesTest;
 
     @Before
     public void setUp() throws Exception {
         airlineService = new AirlineServiceImpl();
+        airplanesRightData = airlineService.createAirplanesList(RIGHT_DATA);
+
+        airplanesTest = new ArrayList<>();
+        Airplane cargoAirplane1 = new CargoAirplane("GH12", "Sukhoi", 850,
+                10000, "C4312", AirplaneType.CARGO, 2, 5000, 245,
+                170, 220);
+        Airplane passengerAirplane1 = new PassengerAirplane("B423", "Airbus", 900,
+                10000, "A320", AirplaneType.PASSENGER, 4, 6000, 195,
+                120, 265);
+
+        Airplane passengerAirplane2 = new PassengerAirplane("S513", "Airbus", 900,
+                10500, "A370", AirplaneType.PASSENGER, 3, 5000, 200,
+                160, 256);
+        Airplane cargoAirplane2 = new CargoAirplane("GH15", "Bombardier", 800,
+                11000, "B234", AirplaneType.CARGO, 4, 5000, 200,
+                180, 220);
+        Airplane passengerAirplane3 = new PassengerAirplane("Y1456", "Boeing", 950,
+                8000, "737", AirplaneType.PASSENGER, 5, 6500, 180,
+                130, 277);
+        airplanesTest.add(cargoAirplane1);
+        airplanesTest.add(passengerAirplane1);
+        airplanesTest.add(passengerAirplane2);
+        airplanesTest.add(cargoAirplane2);
+        airplanesTest.add(passengerAirplane3);
+
     }
 
     @Test
     public void testGetAirplanesQuantity() {
-
-        int airplanes = airlineService.getAirplanesQuantity(RIGHT_DATA);
-
+        int airplanes = airlineService.getAirplanesQuantity(airplanesRightData);
         assertEquals(5, airplanes);
     }
 
-
     @Test
     public void testCreateAirplanesList_success() {
-
-        List<Airplane> airplanes = airlineService.createAirplanesList(RIGHT_DATA);
-
-        assertTrue(airplanes.get(0) instanceof CargoAirplane);
-        assertEquals(airplanes.get(0).getSideNumber(), "GH12");
-        assertEquals(airplanes.get(0).getManufacturer(), "Sukhoi");
-        assertEquals(airplanes.get(0).getModel(), "C4312");
-        assertEquals(airplanes.get(0).getAirplaneType(), AirplaneType.CARGO);
-        assertEquals(airplanes.get(0).getCrewQuantity(), 2);
-        assertEquals(airplanes.get(0).getMaxSpeed(), 850);
-        assertEquals(airplanes.get(0).getMaxAltitude(), 10000);
-        assertEquals(airplanes.get(0).getMaxFlightRange(), 5000);
-        assertEquals(airplanes.get(0).getFuelSupply(), 245);
-        assertEquals(airplanes.get(0).getFuelConsumption(), 170, 0.01);
-        assertEquals(((CargoAirplane) airplanes.get(0)).getCargoCapacity(), 220);
+        List<Airplane> airplanes = airplanesRightData;
+        assertEquals(airplanes.get(0), airplanesTest.get(0));
+        assertEquals(airplanes.get(1), airplanesTest.get(1));
+        assertEquals(airplanes.get(2), airplanesTest.get(2));
+        assertEquals(airplanes.get(3), airplanesTest.get(3));
+        assertEquals(airplanes.get(4), airplanesTest.get(4));
     }
 
 
     @Test(expected = IllegalArgumentException.class)
     public void testCreateAirplanesList_wrongInputData() {
-
-        List<Airplane> airplanes = airlineService.createAirplanesList(WRONG_DATA);
-
-        assertTrue(airplanes.get(0) instanceof CargoAirplane);
-        assertEquals(airplanes.get(0).getSideNumber(), "GH12");
-        assertEquals(airplanes.get(0).getManufacturer(), "Sukhoi");
-        assertEquals(airplanes.get(0).getModel(), "C4312");
-        assertEquals(airplanes.get(0).getAirplaneType(), AirplaneType.CARGO);
-        assertEquals(airplanes.get(0).getCrewQuantity(), 2);
-        assertEquals(airplanes.get(0).getMaxSpeed(), 850);
-        assertEquals(airplanes.get(0).getMaxAltitude(), 10000);
-        assertEquals(airplanes.get(0).getMaxFlightRange(), 5000);
-        assertEquals(airplanes.get(0).getFuelSupply(), 245);
-        assertEquals(airplanes.get(0).getFuelConsumption(), 170, 0.01);
-        assertEquals(((CargoAirplane) airplanes.get(0)).getCargoCapacity(), 220);
+        airplanesWrongData = airlineService.createAirplanesList(WRONG_DATA);
+        List<Airplane> airplanes = airplanesWrongData;
+        assertEquals(airplanes.get(0), airplanesTest.get(0));
+        assertEquals(airplanes.get(1), airplanesTest.get(1));
+        assertEquals(airplanes.get(2), airplanesTest.get(2));
+        assertEquals(airplanes.get(3), airplanesTest.get(3));
+        assertEquals(airplanes.get(4), airplanesTest.get(4));
     }
 
     @Test
     public void testFindTotalCrewCapacity() {
-
-        int crews = airlineService.findTotalCrewCapacity(airlineService.createAirplanesList(RIGHT_DATA));
+        int crews = airlineService.findTotalCrewCapacity(airplanesRightData);
         assertEquals(18, crews);
     }
 
+
     @Test
     public void testFindTotalAirplanesCapacity() {
-
-        int total = airlineService.findTotalAirplanesCapacity(airlineService.createAirplanesList(RIGHT_DATA));
+        int total = airlineService.findTotalAirplanesCapacity(airplanesRightData);
         assertEquals(816, total);
     }
 
     @Test
     public void testFindTotalPassengersCapacity() {
-
-        int total = airlineService.findTotalPassengersCapacity(airlineService.createAirplanesList(RIGHT_DATA));
+        int total = airlineService.findTotalPassengersCapacity(airplanesRightData);
         assertEquals(798, total);
     }
 
     @Test
     public void testFindTotalAirplanesCarryingCapacity() {
-
-        int total = airlineService.findTotalAirplanesCarryingCapacity(airlineService.createAirplanesList(RIGHT_DATA));
+        int total = airlineService.findTotalAirplanesCarryingCapacity(airplanesRightData);
         assertEquals(440, total);
     }
 
     @Test
     public void findPlaneByFuelConsumptionRange() {
-        Airplane airplane = airlineService.findPlaneByFuelConsumptionRange(airlineService.createAirplanesList(RIGHT_DATA), 100, 200);
+        Airplane airplane = airlineService.findPlaneByFuelConsumptionRange(airplanesRightData, 100, 200);
         assertEquals(airplane.getFuelConsumption(), 120.0, 0.01);
     }
 }
