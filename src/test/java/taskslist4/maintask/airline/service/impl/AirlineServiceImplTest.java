d2e3f7ea9b40bf0fac29072d4f624068b0fac9e3
Viktar Mudrahelau <prespo@tut.by>
2019-10-18 12:50:14 +0300

    added tests

--- src/test/java/taskslist4/maintask/airline/service/impl/AirlineServiceImplTest.java
+++ src/test/java/taskslist4/maintask/airline/service/impl/AirlineServiceImplTest.java
@@ -2,10 +2,11 @@ package taskslist4.maintask.airline.service.impl;
 
 import org.junit.Before;
 import org.junit.Test;
-import taskslist4.maintask.airline.model.Airplane;
-import taskslist4.maintask.airline.model.AirplaneType;
-import taskslist4.maintask.airline.model.CargoAirplane;
-import taskslist4.maintask.airline.model.PassengerAirplane;
+import taskslist4.maintask.airline.comparator.AirplaneBrandComparator;
+import taskslist4.maintask.airline.comparator.AirplaneCrewCapacityComparator;
+import taskslist4.maintask.airline.comparator.AirplaneFuelConsumptionComparator;
+import taskslist4.maintask.airline.comparator.AirplanePassengersCapacityComparator;
+import taskslist4.maintask.airline.model.*;
 import taskslist4.maintask.airline.service.AirlineService;
 
 import java.util.ArrayList;
@@ -112,4 +113,137 @@ public class AirlineServiceImplTest {
         Airplane airplane = airlineService.findPlaneByFuelConsumptionRange(airplanesRightData, 100, 200);
         assertEquals(airplane.getFuelConsumption(), 120.0, 0.01);
     }
+
+    /**
+     * Compares airplanes by brand alphabetically
+     */
+    @Test
+    public void testAirplaneBrandComparator() {
+        List<Airplane> sortedAirplanes = new ArrayList<>();
+        Airplane cargoAirplane1 = new CargoAirplane("GH12", "Sukhoi", 850,
+                10000, "C4312", AirplaneType.CARGO, 2, 5000, 245,
+                170, 220);
+        Airplane passengerAirplane1 = new PassengerAirplane("B423", "Airbus", 900,
+                10000, "A320", AirplaneType.PASSENGER, 4, 6000, 195,
+                120, 265);
+        Airplane passengerAirplane2 = new PassengerAirplane("S513", "Airbus", 900,
+                10500, "A370", AirplaneType.PASSENGER, 3, 5000, 200,
+                160, 256);
+        Airplane cargoAirplane2 = new CargoAirplane("GH15", "Bombardier", 800,
+                11000, "B234", AirplaneType.CARGO, 4, 5000, 200,
+                180, 220);
+        Airplane passengerAirplane3 = new PassengerAirplane("Y1456", "Boeing", 950,
+                8000, "737", AirplaneType.PASSENGER, 5, 6500, 180,
+                130, 277);
+
+        sortedAirplanes.add(passengerAirplane1);
+        sortedAirplanes.add(passengerAirplane2);
+        sortedAirplanes.add(passengerAirplane3);
+        sortedAirplanes.add(cargoAirplane2);
+        sortedAirplanes.add(cargoAirplane1);
+
+        airplanesRightData.sort(new AirplaneBrandComparator());
+
+        assertEquals(sortedAirplanes, airplanesRightData);
+    }
+
+    /**
+     * Compares airplanes by crew capacity from larger to lower capacity
+     */
+    @Test
+    public void testAirplaneCrewCapacityComparator() {
+        List<Airplane> sortedAirplanes = new ArrayList<>();
+        Airplane cargoAirplane1 = new CargoAirplane("GH12", "Sukhoi", 850,
+                10000, "C4312", AirplaneType.CARGO, 2, 5000, 245,
+                170, 220);
+        Airplane passengerAirplane1 = new PassengerAirplane("B423", "Airbus", 900,
+                10000, "A320", AirplaneType.PASSENGER, 4, 6000, 195,
+                120, 265);
+        Airplane passengerAirplane2 = new PassengerAirplane("S513", "Airbus", 900,
+                10500, "A370", AirplaneType.PASSENGER, 3, 5000, 200,
+                160, 256);
+        Airplane cargoAirplane2 = new CargoAirplane("GH15", "Bombardier", 800,
+                11000, "B234", AirplaneType.CARGO, 4, 5000, 200,
+                180, 220);
+        Airplane passengerAirplane3 = new PassengerAirplane("Y1456", "Boeing", 950,
+                8000, "737", AirplaneType.PASSENGER, 5, 6500, 180,
+                130, 277);
+
+        sortedAirplanes.add(passengerAirplane3);
+        sortedAirplanes.add(passengerAirplane1);
+        sortedAirplanes.add(cargoAirplane2);
+        sortedAirplanes.add(passengerAirplane2);
+        sortedAirplanes.add(cargoAirplane1);
+
+        airplanesRightData.sort(new AirplaneCrewCapacityComparator());
+
+        assertEquals(sortedAirplanes, airplanesRightData);
+    }
+
+    /**
+     * Compares airplanes by passenger capacity from larger to smaller
+     */
+    @Test
+    public void testAirplanePassengerCapacityComparator() {
+        List<Airplane> sortedAirplanes = new ArrayList<>();
+        Airplane cargoAirplane1 = new CargoAirplane("GH12", "Sukhoi", 850,
+                10000, "C4312", AirplaneType.CARGO, 2, 5000, 245,
+                170, 220);
+        Airplane passengerAirplane1 = new PassengerAirplane("B423", "Airbus", 900,
+                10000, "A320", AirplaneType.PASSENGER, 4, 6000, 195,
+                120, 265);
+        Airplane passengerAirplane2 = new PassengerAirplane("S513", "Airbus", 900,
+                10500, "A370", AirplaneType.PASSENGER, 3, 5000, 200,
+                160, 256);
+        Airplane cargoAirplane2 = new CargoAirplane("GH15", "Bombardier", 800,
+                11000, "B234", AirplaneType.CARGO, 4, 5000, 200,
+                180, 220);
+        Airplane passengerAirplane3 = new PassengerAirplane("Y1456", "Boeing", 950,
+                8000, "737", AirplaneType.PASSENGER, 5, 6500, 180,
+                130, 277);
+
+        sortedAirplanes.add(passengerAirplane3);
+        sortedAirplanes.add(passengerAirplane1);
+        sortedAirplanes.add(passengerAirplane2);
+        sortedAirplanes.add(cargoAirplane1);
+        sortedAirplanes.add(cargoAirplane2);
+
+        airplanesRightData.sort(new AirplanePassengersCapacityComparator());
+
+        assertEquals(sortedAirplanes, airplanesRightData);
+    }
+
+    /**
+     * Compares airplanes by fuel consumption from smaller to larger
+     */
+    @Test
+    public void testAirplaneFuelConsumptionComparator() {
+        List<Airplane> sortedAirplanes = new ArrayList<>();
+        Airplane cargoAirplane1 = new CargoAirplane("GH12", "Sukhoi", 850,
+                10000, "C4312", AirplaneType.CARGO, 2, 5000, 245,
+                170, 220);
+        Airplane passengerAirplane1 = new PassengerAirplane("B423", "Airbus", 900,
+                10000, "A320", AirplaneType.PASSENGER, 4, 6000, 195,
+                120, 265);
+        Airplane passengerAirplane2 = new PassengerAirplane("S513", "Airbus", 900,
+                10500, "A370", AirplaneType.PASSENGER, 3, 5000, 200,
+                160, 256);
+        Airplane cargoAirplane2 = new CargoAirplane("GH15", "Bombardier", 800,
+                11000, "B234", AirplaneType.CARGO, 4, 5000, 200,
+                180, 220);
+        Airplane passengerAirplane3 = new PassengerAirplane("Y1456", "Boeing", 950,
+                8000, "737", AirplaneType.PASSENGER, 5, 6500, 180,
+                130, 277);
+
+        sortedAirplanes.add(passengerAirplane1);
+        sortedAirplanes.add(passengerAirplane3);
+        sortedAirplanes.add(passengerAirplane2);
+        sortedAirplanes.add(cargoAirplane1);
+        sortedAirplanes.add(cargoAirplane2);
+
+        airplanesRightData.sort(new AirplaneFuelConsumptionComparator());
+
+        assertEquals(sortedAirplanes, airplanesRightData);
+    }
+
 }
